5ef703ba2be36b2748bdd847d719d737d1d0a4ae
reemer30 <aidan.lenhart@gmail.com>
2019-08-13 23:00:51 -0700

    update comments and icon

---
+++ js/replace_header.js
@@ -0,0 +1,4 @@
+let images = document.getElementsByTagName('img')
+console.log("image replaced" + images[0].title);
+images[0].src = "https://polyplanner.calpoly.edu/udirect/laf/themes/udirect/images/cplogo.png"
+
