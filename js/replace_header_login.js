fb1c1c109fb28d76f16f81510b665eb2998a8986
reemer30 <aidan.lenhart@gmail.com>
2019-10-14 14:48:18 -0700

    added fix for new login page

---
+++ js/replace_header_login.js
@@ -0,0 +1,5 @@
+var a = document.getElementsByClassName("header");
+a[0].innerHTML = ""
+b = document.createElement('img')
+b.src = "https://polyplanner.calpoly.edu/udirect/laf/themes/udirect/images/cplogo.png"
+a[0].appendChild(b)
---
+++ js/replace_icon_login.js
@@ -0,0 +1,1 @@
+document.querySelector('link[rel*="icon"]').href = "https://raw.githubusercontent.com/reemer30/poly-logo-fix/master/icons/favicon-96x96.ico"
